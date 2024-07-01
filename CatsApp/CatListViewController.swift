ade83be723988af13302d387af0f7f8a47000ebd
Miguel <mikisaraviaios@gmail.com>
2024-07-01 15:26:55 -0400

    deleted default controller and create the main view

---
+++ CatsApp/CatListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CatListViewController.swift
+//  CatsApp
+//
+//  Created by Miguel Saravia on 1/7/24.
+//
+
+import UIKit
+
+class CatListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
