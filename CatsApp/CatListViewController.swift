3c02c35f1814e12dc623f20cbccdfbbfd4865d34
Miguel <mikisaraviaios@gmail.com>
2024-07-04 10:52:10 -0400

    - deleted useless code

--- CatsApp/CatListViewController.swift
+++ CatsApp/CatListViewController.swift
@@ -6,14 +6,11 @@
 //
 
 import UIKit
-import Networking
 
 class CatListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-      let demo = Networking()
-      print(demo.foo)
         // Do any additional setup after loading the view.
     }
 }
