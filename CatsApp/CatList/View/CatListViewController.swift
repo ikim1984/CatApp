3085aeb661f7ce644e4803d0840a8706283ba4df
Miguel <mikisaraviaios@gmail.com>
2024-07-04 20:05:00 -0400

    - create a delegate for reload data - fixed image load - created spinner loader in component

--- CatsApp/CatList/View/CatListViewController.swift
+++ CatsApp/CatList/View/CatListViewController.swift
@@ -14,22 +14,38 @@ final class CatListViewController: UIViewController {
  //MARK:- Variables
   private let viewModel = CatsListViewModel()
   
+  private let limit = 20
+  private var skip = 0
+  
   //MARK:- Livecycle
   override func viewDidLoad() {
     super.viewDidLoad()
+    catsCollectionView.collectionDelegate = self
     loadCats()
   }
   
   //MARK:- Functions
   func loadCats() {
-    viewModel.fetchCats(limit: 20, skip: 0) { result in
+    viewModel.fetchCats(limit: 20, skip: skip) { result in
       switch result {
       case let .success(response):
-        let requestModel: [CatRequest] = response.compactMap { CatRequest(image: $0.id ?? "", tag: $0.tags ?? []) }
+        let requestModel: [CatCellModel] = response.compactMap { CatCellModel(image: $0.id ?? "", 
+                                                                              tag: $0.tags ?? []) }
         self.catsCollectionView.updateCollection(dataList: requestModel)
       case let .failure(error):
         print(error.localizedDescription)
       }
     }
+  }
+}
+
+extension CatListViewController: CatsConfig {
+  func checkLastId(isLast: Bool) {
+    skip += limit
+    loadCats()
+  }
+  
+  func onTapSelector(data: CatCellModel) {
+    print("********* TAP SELECTED ***********\n", data)
   }
 }
